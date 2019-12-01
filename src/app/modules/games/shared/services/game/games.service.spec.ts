1bb1c47dc56bb37e79c33c9a8b29d7f708db88cf
Radmila Mitrevska <radmila@stativa.com.mk>
2019-12-01 15:01:32 +0100

    tests for GamesService

--- src/app/modules/games/shared/services/game/games.service.spec.ts
+++ src/app/modules/games/shared/services/game/games.service.spec.ts
@@ -3,10 +3,38 @@ import { TestBed } from '@angular/core/testing';
 import { GamesService } from './games.service';
 
 describe('GamesService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [GamesService]
+    });
+  });
 
   it('should be created', () => {
     const service: GamesService = TestBed.get(GamesService);
     expect(service).toBeTruthy();
   });
+
+  describe('method #searchGames', () => {
+    it('test search query match', () => {
+      const service: GamesService = TestBed.get(GamesService);
+      (service as any)._allGames = gamesJson;
+      const results = service.searchGames('wol');
+
+      expect(results.length).toBe(1);
+      expect(results[0].name).toBe('WolfsBet ');
+    });
+  });
+
 });
+
+const gamesJson = [
+  {
+    name: 'WolfsBet ',
+  },
+  {
+    name: 'FoxBet ',
+  },
+  {
+    name: 'BearBet ',
+  },
+];
